c08d5fa0505b20930f351d52c6f58856db359ef3
Henrique Ruaz <rickruaz55@gmail.com>
2024-03-27 20:23:37 +0000

    L3

--- L3/Program.cs
+++ L3/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1{
+namespace L3{
     internal class Program{
         static void Main(string[] args){
 
