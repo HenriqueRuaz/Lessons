aeb3e2794985d28bf28b93bf42105f345c8e1b1e
Henrique Ruaz <rickruaz55@gmail.com>
2024-03-27 19:12:54 +0000

    L1, L2

---
+++ L1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Block of code; Fuction
+            Console.WriteLine("Hello World"); //Instruction to Write; Console -> Output "Hello World"
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("__________ ");            //For Fun :)
+            Console.WriteLine("| /////// |");
+            Console.WriteLine("|         |");
+            Console.WriteLine("|-(O)-(O)-|");
+            Console.WriteLine("|         |");
+            Console.WriteLine("| _     _ |");
+            Console.WriteLine("|  -----  |");
+            Console.WriteLine("|_________|");
+
+            Console.ReadLine();               //Instroction to Read; Console -> Output static screen
+
+        }
+    }
+}
---
+++ L2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name = "Uriel";                  //Variable type string
+            string Di = "direct", In = "indirect";  //Variable type string
+            // Little history, nothing more ;)
+            Console.WriteLine(Name + " is a " + Di + " ascendent!");
+            Console.WriteLine("And He will give up of being a " + Di + " ascendent.");
+            Console.WriteLine("That will cause 50 years to " + Name + " become an " + In + " ascendent.");
+            Console.WriteLine("But the determination of one man, change " + Name + " perspective.");
+            Console.WriteLine("And now " + Name + " will do evry thing to be a " + Di + " ascendent.");
+            Console.WriteLine("It is time for the sun rise again!");
+
+            Console.ReadLine();
+        }
+    }
+}
