3bcfb4ec165f551de5192cff635c0cbe2069b4b4
Henrique Ruaz <rickruaz55@gmail.com>
2024-03-29 09:15:40 +0000

    L5/L6

---
+++ L5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L5{
+    internal class Program{
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(12 + 21);
+            Console.WriteLine(12 - 22);
+            Console.WriteLine(12 * 23);
+            Console.WriteLine(12 / 24);
+            Console.WriteLine(Math.Abs(-12));       //Absolute
+            Console.WriteLine(Math.Pow(12, 2));
+            Console.WriteLine(Math.Sqrt(9));
+            Console.WriteLine(Math.Max(100, 10));
+            Console.WriteLine(Math.Min(100, 10));
+            Console.WriteLine(Math.Round(5.7));
+            Console.WriteLine(Math.Round(5.4));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ L6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L6{
+    internal class Program{
+        static void Main(string[] args){
+
+            Console.WriteLine("Hello");
+            Console.Write("What is your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Hello " + name + ", you have a nice name!");
+
+            Console.WriteLine("");
+            Console.WriteLine("Enter to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
