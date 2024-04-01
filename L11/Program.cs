c845a4aa88891b08d95ece1a26ce8fa11c3bd21a
Henrique Ruaz <rickruaz55@gmail.com>
2024-04-01 21:57:22 +0100

    L11

---
+++ L11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L11{
+    internal class Program{
+        static void Main(string[] args){
+
+            Console.WriteLine(Cube(2));
+
+            Console.WriteLine("");
+            Console.WriteLine("Press Enter to exit!");
+            Console.ReadLine();
+        }
+        static double Cube(int k1){
+
+
+            double n1 = Math.Pow(k1, 3);
+            return n1;
+
+            
+        }
+
+    }
+}
