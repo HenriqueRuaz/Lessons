aacc553b42e3ac50fe876314f3b84bfbfa75e360
Henrique Ruaz <rickruaz55@gmail.com>
2024-04-08 21:36:52 +0100

    L22

---
+++ L22/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L22{
+	internal class Security{
+
+		public string UserName;
+		public string EnterPassWord;
+	}
+}
---
+++ L22/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L22{ 
+	internal class Program{
+		static void Main(string[] args){
+			
+			string Name; 
+			string Pass;
+
+
+			Security Login = new Security(); //object
+
+
+			Console.WriteLine("-Register ");
+			Console.WriteLine("");
+
+			Console.Write("Enter your User_Name: ");
+			Name = Console.ReadLine();
+			Login.UserName = Name;
+
+			Console.Write("Enter your Password: ");
+			Pass = Console.ReadLine();
+			Login.EnterPassWord = Pass;
+
+			
+			Console.WriteLine("Your User_Name is: " + Login.UserName);
+			Console.WriteLine("Your Password is: " + Login.EnterPassWord);
+			
+
+			Console.WriteLine("successful login");
+			
+			
+			Console.WriteLine("");
+			Console.WriteLine("Press Enter to Exit!");
+			Console.ReadLine();
+		}
+	}
+}
