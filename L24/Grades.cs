0ff5288911eb40aac8c1c12403ca3091c270b732
Henrique Ruaz <rickruaz55@gmail.com>
2024-04-13 21:06:09 +0100

    L24

---
+++ L24/Grades.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L24{
+	internal class Grades{
+
+		public string name;
+		public double grades;
+
+		public Grades (string  Sname, double Sgrades)
+		{
+			name = Sname;
+			grades = Sgrades;
+			Console.WriteLine("Student : " + Sname + " " + grades);
+		}
+		//entrance to university
+		public bool entrance (){
+			
+			if (grades < 3.5){
+
+				return false;
+			}else{
+				return true;
+			}
+			
+		}
+	}
+}
---
+++ L24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L24{
+	internal class Program{
+		static void Main(string[] args){
+
+			
+			Grades student1 = new Grades("John", 8.4);
+			Console.WriteLine(student1.entrance());
+
+			Grades student2 = new Grades("Sophie", 5.5);
+			Console.WriteLine(student2.entrance());
+			
+			Grades student3 = new Grades("Arthur", 6.7);
+			Console.WriteLine(student3.entrance());
+
+			Grades student4 = new Grades("Alfred", 2.2);
+			Console.WriteLine(student4.entrance());
+
+
+			Console.WriteLine("");
+			Console.WriteLine("Press Enter to Exit!");
+			Console.ReadLine();
+		}
+	}
+}
