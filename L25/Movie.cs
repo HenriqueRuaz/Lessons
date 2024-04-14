4d6ebe0c9806527c38c6726faa15f3f20fbe5918
Henrique Ruaz <rickruaz55@gmail.com>
2024-04-14 15:43:30 +0100

    L25

---
+++ L25/Movie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L25{
+	internal class Movie{
+
+		public string Name;
+		public string Director;
+		private string Rating;
+
+		public Movie(string aName, string aDirector, string aRating){	
+
+			Name = aName;
+			Director = aDirector;
+			realRating = aRating;
+			
+		}
+		public string realRating{ 
+
+			get { return Rating; }
+			set { 
+				if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR"){
+
+					Rating = value;
+				}else{
+
+					Rating = "NR";
+				 }
+			}
+		}
+	}
+}
---
+++ L25/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L25{
+	internal class Program{
+		static void Main(string[] args){
+			
+			Movie movie1 = new Movie("The Avangers", "Joss Whedon", "R");
+			Movie movie2 = new Movie("Shrek", "Adam Adomson", "PG");
+			
+			Console.WriteLine(movie1.realRating);
+
+			Console.WriteLine("");
+			Console.WriteLine("Press Enter to Exit!");
+			Console.ReadLine();
+		}
+	}
+}
